d7dd079064202a8f71191457c2ca07d0e0a2cdac
Benjamin Voran <bvoran@gmail.com>
2012-02-07 15:30:52 -0600

    iframe sponsor media ad

--- js/llc-player.js
+++ js/llc-player.js
@@ -1463,7 +1463,7 @@ var llc = {
 						$("#llc_playerFrame").trigger('resize');
 						$("#buyButton").show();
 						
-						/* Set up ad */
+						/* ######### Set up ad */
 						if (!document.phpAds_used) document.phpAds_used = ',';
 						phpAds_random = new String (Math.random()); phpAds_random = phpAds_random.substring(2,11);
 						
@@ -1472,11 +1472,17 @@ var llc = {
 						adjs += "&exclude=" + document.phpAds_used;
 						
 						//if ($.jPlayer.platform.tablet || $.jPlayer.platform.mobile) {
+							
 							var randomString = Math.round(Math.random() * 555955);
-							var manualUrl = "<a href='http://content.multiview.com/adclick.php?n="+randomString+"' target='_blank'>";
-							manualUrl = manualUrl + "<img src='http://content.multiview.com/adview.php?what=zone:"+llc.pres.sponsorZoneId+"&n="+randomString+"' border='0' alt='' /></a>";
+							
+//							var manualUrl = "<a href='http://content.multiview.com/adclick.php?n="+randomString+"' target='_blank'>";
+//							manualUrl += "<img src='http://content.multiview.com/adview.php?what=zone:"+llc.pres.sponsorZoneId+"&n="+randomString+"' border='0' alt='' /></a>";
+							
+							var manualUrl = "<iframe id='a1df8b12' name='a1df8b12' src='http://content2.multiview.com/www/delivery/afr.php?refresh=20&amp;zoneid="+1469+"&amp;cb="+randomString+"' frameborder='0' scrolling='no' width='120' height='60'><a href='http://content2.multiview.com/www/delivery/ck.php?n=a7af83b1&amp;cb=INSERT_RANDOM_NUMBER_HERE' target='_blank'><img src='http://content2.multiview.com/www/delivery/avw.php?zoneid="+1469+"&amp;cb="+randomString+"&amp;n=a7af83b1' border='0' alt='' /></a></iframe>"
 							var defaultAd = '<a href="http://multiview.com/multiview_media.html" target="_blank" style="display:block; height:60px;"></a>';
-							   $("#ad_sponsored_box").html(manualUrl || defaultAd);
+							
+							$("#ad_sponsored_box").html(manualUrl || defaultAd);
+							
 //						}else{	
 //							$.getScript(adjs, function(){
 //								var defaultAd = '<a href="http://multiview.com/multiview_media.html" target="_blank" style="display:block; height:60px;"></a>';
